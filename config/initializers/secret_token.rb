e49864055b26d9ea19c01917cb9d41f829a01af6
Roger Vandervort <rvandervort@gmail.com>
2013-01-16 10:10:03 -0500

    create initial application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BattleshipAlgorithm::Application.config.secret_token = '5079f51cf950d804a735055f0f34b4ff0cf1df247793067bba001d069529e46a09053e74227e147e2f21d7867d9f15d696bd6d14bebe1e7ab70c945f966af8fe'
